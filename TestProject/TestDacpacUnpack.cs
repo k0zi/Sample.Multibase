2e168f8a8dff3123c551a5cfa9e7d7e189b998de
David Kozma <k0zi@outlook.com>
2025-05-10 18:08:30 +0200

    Rewrite with new versions

--- TestProject/TestDacpacUnpack.cs
+++ TestProject/TestDacpacUnpack.cs
@@ -6,35 +6,22 @@ namespace TestProject
     public class TestDacpacUnpack
     {
 #if DEBUG
-        private readonly string dacpacPath = @$"..\..\..\..\Beta\bin\Debug\Beta.dacpac";
-        private readonly string dacpacPathSdk = @$"..\..\..\..\Beta.Sdk\bin\Debug\Beta.Sdk.dacpac";
-        private readonly string dacpacPathExtended = @$"..\..\..\..\Extended\bin\Debug\Extended.dacpac";
+
+        private readonly string dacpacPathSdk = Path.Combine(new[]
+            { "..", "..", "..", "..", "Beta.Sdk", "bin", "Debug", "Beta.Sdk.dacpac" });
+        private readonly string dacpacPathExtended = Path.Combine(new[]
+            { "..", "..", "..", "..", "Extended", "bin", "Debug", "Extended.dacpac" });
 #else
-        private readonly string dacpacPath = @$"..\..\..\..\Beta\bin\Release\Beta.dacpac";
         private readonly string dacpacPathSdk = @$"..\..\..\..\Beta.Sdk\bin\Release\Beta.Sdk.dacpac";
         private readonly string dacpacPathExtended = @$"..\..\..\..\Extended\bin\Release\Extended.dacpac";
 #endif
         [Fact]
         public void Is_Dacpacs_Exists()
         {
-            var result = File.Exists(dacpacPath) 
-                && File.Exists(dacpacPathSdk) 
+            var current = Directory.GetCurrentDirectory();
+            var result = File.Exists(dacpacPathSdk) 
                 && File.Exists(dacpacPathExtended);
             Assert.True(result);
-        }
-
-        [Fact]
-        public void Unpack_Should_Not_Throws_Exception()
-        {
-            var dacpac = DacPackage.Load(dacpacPath);
-            if(Directory.Exists(dacpac.Name))
-            {
-                Directory.Delete(dacpac.Name, true);
-            }
-            var directory = Directory.CreateDirectory(dacpac.Name);
-            Action act = () => dacpac.Unpack(directory.FullName);
-            act.Should()
-                .NotThrow();
         }
 
         [Fact]
