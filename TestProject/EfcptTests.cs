089fec3af03c6e11f375898e4ff789af046c3120
David Kozma <k0zi@outlook.com>
2025-06-09 09:17:05 +0200

    Using merge dacpacs

--- TestProject/EfcptTests.cs
+++ TestProject/EfcptTests.cs
@@ -12,47 +12,68 @@ namespace TestProject
 #else
         private const string CONFIGURATION = "Release";
 #endif
+        private const string Alpha = "Alpha.Sdk.dacpac";
+        private const string Beta = "Beta.Sdk.dacpac";
+        private const string Gamma = "Gamma.Sdk.dacpac";
+        private readonly string alphaPath = Path.Combine(new[]
+            { "..", "..", "..", "..", "Alpha.Sdk", "bin", CONFIGURATION, Alpha });
         private readonly string betaPath = Path.Combine(new[]
-            { "..", "..", "..", "..", "Beta.Sdk", "bin", CONFIGURATION, "Beta.Sdk.dacpac" });
+            { "..", "..", "..", "..", "Beta.Sdk", "bin", CONFIGURATION, Beta });
         private readonly string gammaPath = Path.Combine(new[]
-            { "..", "..", "..", "..", "Gamma.Sdk", "bin", CONFIGURATION, "Gamma.Sdk.dacpac" });
+            { "..", "..", "..", "..", "Gamma.Sdk", "bin", CONFIGURATION, Gamma });
+        
+
+        public EfcptTests()
+        {
+            File.Copy(alphaPath,Alpha, true);
+            File.Copy(betaPath,Beta, true);
+            File.Copy(gammaPath,Gamma, true);
+        }
 
         [Fact]
         public void Is_Beta_Folder_Exists()
         {
-            var directory = new FileInfo(betaPath).Directory.FullName;
+            var directory = new FileInfo(Beta).Directory.FullName;
             Assert.True(Directory.Exists(directory));
         }
 
         [Fact]
         public void Is_Beta_Dacpac_Exists()
         {
-            var file = new FileInfo(betaPath).FullName;
+            var file = new FileInfo(Beta).FullName;
             Assert.True(File.Exists(file));
         }
         
         [Fact]
         public void Is_Gamma_Folder_Exists()
         {
-            var directory = new FileInfo(gammaPath).Directory.FullName;
+            var directory = new FileInfo(Gamma).Directory.FullName;
             Assert.True(Directory.Exists(directory));
         }
 
         [Fact]
         public void Is_Gamma_Dacpac_Exists()
         {
-            var file = new FileInfo(gammaPath).FullName;
+            var file = new FileInfo(Gamma).FullName;
             Assert.True(File.Exists(file));
+        }
+
+        [Fact]
+        public void Exists_Efcpt_On_Path()
+        {
+            var file = "efcpt-beta-config.json";
+            var file2 = "efcpt-gamma-config.json";
+            Assert.True(File.Exists(file) && File.Exists(file2));
         }
 
         [Fact]
         public void Run_Efcpt_On_Beta_WO_Error()
         {
-            var file = new FileInfo(betaPath).FullName;
+            var file = new FileInfo(Beta).FullName;
             var processStartInfo = new ProcessStartInfo
             {
                 FileName = "efcpt",
-                Arguments = $"\"{file}\" mssql",
+                Arguments = $"\"{file}\" mssql -i \"efcpt-beta-config.json\"",
                 UseShellExecute = false,
                 RedirectStandardOutput = true
             };
@@ -66,11 +87,11 @@ namespace TestProject
         [Fact]
         public void Run_Efcpt_On_Gamma_WO_Error()
         {
-            var file = new FileInfo(gammaPath).FullName;
+            var file = new FileInfo(Gamma).FullName;
             var processStartInfo = new ProcessStartInfo
             {
                 FileName = "efcpt",
-                Arguments = $"\"{file}\" mssql",
+                Arguments = $"\"{file}\" mssql -i \"efcpt-gamma-config.json\"",
                 UseShellExecute = false,
                 RedirectStandardOutput = true
             };
